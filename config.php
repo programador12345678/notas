f358d012ed8b9fb7e542430a6faa44f69e966d56
programador12345678 <159459789+programador12345678@users.noreply.github.com>
2025-12-28 16:37:06 -0600

    config.php

--- config.php
+++ config.php
@@ -1,16 +1,15 @@
 <?php
-$host = 'localhost';
-$db   = 'sistema_notas';
-$user = 'root';
-$pass = ''; // Tu contraseña de MySQL
-$charset = 'utf8mb4';
+// Tus credenciales de Neon
+$host = 'ep-shiny-unit-adiqn7rh-pooler.c-2.us-east-1.aws.neon.tech';
+$db   = 'neondb';
+$user = 'neondb_owner';
+$pass = 'npg_px4kLbqlJ6rB';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 try {
+    $dsn = "pgsql:host=$host;port=5432;dbname=$db;sslmode=require";
     $pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-} catch (\PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
+    session_start();
+} catch (PDOException $e) {
+    die("Error conectando a Neon: " . $e->getMessage());
 }
-
-session_start();
 ?>
